47a992319bde877817e52aeb3717a13cc219aa37
Apolo <saggittarius.will@gmail.com>
2020-10-18 11:49:25 -0500

    Titulo modificado app.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>delivery appg</Text>
+      <Text>delivery apdpg</Text>
       <StatusBar style="auto" />
     </View>
   );
